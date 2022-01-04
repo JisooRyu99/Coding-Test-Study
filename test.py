688d9fb9209d9f6fe31fcc35132b34ed0ab7f793
jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-01-04 22:46:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 def solution(s):
-    stack=[]
+    stack=[]    # 빈 list 만들어주기
     for i in s:
-        if not stack:
+        if not stack:   
             stack.append(i)
             continue
 
-        if stack[-1]==i:
+        if stack[-1]==i:    # stack의 마지막 원소와 i와 같으면 pop
             stack.pop()
-        else: stack.append(i)
+        else: stack.append(i)   # 같지 않으면 stack에 넣기
 
     if not stack:
-        return 1
-    else: return 0    
+        return 1    # stack에 아무것도 없으면 1
+    else: return 0    # stack에 하나라도 있으면 0
