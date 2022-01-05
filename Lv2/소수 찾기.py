34c84e32ef77675de87e7b9418b25150b8277ce1
JisooRyu99 <cussion09@naver.com>
2022-01-05 23:09:28 +0900

    Lv2.소수 찾기

--- Lv2/소수 찾기.py
+++ Lv2/소수 찾기.py
@@ -1,10 +1,10 @@
 from itertools import permutations
 
-def prime_num(num):
+def prime_num(num):         # 소수 판별
     if num<2:
         return False
     for i in range(2,num):
-        if num%i==0:
+        if num%i==0:   
             return False
     return True
 
@@ -13,11 +13,11 @@ def solution(numbers):
     stack=[]
     temp=[]
     for i in range(1,len(numbers)+1):
-        temp.extend(permutations(numbers, i))
-        stack = [int(''.join(i)) for i in temp]
+        temp.extend(permutations(numbers, i)) # numbers을 가지고 순열 만들기
+        stack = [int(''.join(i)) for i in temp] 
+        # temp 저장된 원소 한 숫자로 만들기
+        # ex) ('9','1') -> [91]
     for i in set(stack):
-        if prime_num(i):
-            print(i)
-            
+        if prime_num(i):    # 소수 판별
             answer+=1
     return answer
