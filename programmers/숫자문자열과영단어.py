0b53c1fb3eb9d5fe3b4c67d428ee9c6e30fee9cd
JisooRyu99 <cussion09@naver.com>
2022-01-07 23:07:53 +0900

    Lv1

---
+++ programmers/숫자문자열과영단어.py
@@ -0,0 +1,17 @@
+dic = {'0' : 'zero',
+        '1' : 'one',
+        '2' : 'two',
+        '3' : 'three',
+        '4' : 'four',
+        '5' : 'five',
+        '6' : 'six',
+        '7' : 'seven',
+        '8' : 'eight',
+        '9' : 'nine'      
+      }
+
+def solution(s):
+    answer =''
+    for key, value in dic.items():
+        s = s.replace(value,key)
+    return int(s)
