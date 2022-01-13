5a92701aa33b557314a1ea9195b8a21054279481
jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-01-13 16:37:54 +0900

    Update 체육복.py

--- Lv1/체육복.py
+++ Lv1/체육복.py
@@ -1,23 +1,22 @@
-def solution(n, lost, reserve):
-    only = n-len(set(reserve)|set(lost))
-    # 자기 체육복만 있는 학생
-    reserve_ = set(reserve)-set(lost)
-    # 여벌이 있는 학생 (도난 당한 학생과 같은 사이즈 제외)
-    lost_ = list(set(lost) - set(reserve))
-    # 여벌이 있는 학생과 같은 사이즈가 없는 도난 당한 학생
-    reserve.sort()
-    lost.sort()
-
-    for i in reserve_:
-        front = i - 1 
-        back = i + 1 
-        if front in lost_:  
-            # 앞 학생이 여벌이 있으면 lost_에서 제거
-            lost_.remove(front)
-        elif back in lost_:
-            lost_.remove(back)
-            # 뒷 학생이 여벌이 있으면 lost_에서 제거
-    return n-len(lost_)
 
 # lost_ = [(i) for i in lost for j in reserve if (i-j)==1 ]
 # 다시 시도해보기
+
+
+def solution(n,lost, reserve):
+
+    only = n-len(set(reserve)|set(lost))  # 자기것
+    only_reserve = list(set(reserve)-set(lost)) # 여벌이 있는 학생 (도난 당한 학생과 같은 사이즈 제외)
+    only_lost = list(set(lost)-set(reserve)) # 여벌이 있는 학생과 같은 사이즈가 없는 도난 당한 학생
+    only_reserve.sort()
+    only_lost.sort()
+    
+    for i in only_reserve:      # 앞 뒤로 학생들 
+        left = i-1
+        right = i+1
+        if left in only_lost:
+            only_lost.remove(left)
+        elif right in only_lost:
+            only_lost.remove(right)
+            
+    return n-len(only_lost)
