32e92914a9c10d42ae480959118519c088f4dd18
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-01-21 18:06:09 +0900

    Update 프린터.py

--- Lv2/프린터.py
+++ Lv2/프린터.py
@@ -20,3 +20,23 @@ def solution(priorities,location):
     return ans
 
 solution([1, 1, 9, 1, 1, 1], 0)
+
+
+-----------------------------------------------------------------------------
+# deque 
+
+def solution(priorities, location):
+    answer = 0
+    from collections import deque
+
+    d = deque([(v,i) for i,v in enumerate(priorities)])
+
+    while len(d):
+        item = d.popleft()
+        if d and max(d)[0] > item[0]:
+            d.append(item)
+        else:
+            answer += 1
+            if item[1] == location:
+                break
+    return answer
