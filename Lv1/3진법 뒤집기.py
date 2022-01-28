77bdff6250c5dc5d4bf0d73b2ff19070ced193a4
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-01-29 00:09:28 +0900

    Update 3진법 뒤집기.py

--- Lv1/3진법 뒤집기.py
+++ Lv1/3진법 뒤집기.py
@@ -1,22 +1,16 @@
-def ternary(n,q):   # 3진법 만들기
+def ternary(n,q):       # 3진법
     rev_base=''
     while n:
         n,mod = divmod(n,q)
         rev_base += str(mod)
     return rev_base[::-1]
-  
 def solution(n):
     answer = 0
-    cnt=3
     s =(ternary(n,3))
-    while cnt>0: 
-        m=''
-        for digit in s:   # 뒤집기
-            m=digit+m
-        s=m
-        cnt-=1
-    return int(s,3)       # 10진법 return 
-            
+    m=''
+    for digit in s:     # 숫자 뒤집기
+        m=digit+m
+    return int(m,3)
 
   --------------------------------------------------------------------------
 # 다른 사람의 풀이
