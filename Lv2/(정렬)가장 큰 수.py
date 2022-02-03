bdbbd44bf54ff722da272679c2f980dbd4080388
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-02-03 16:47:35 +0900

    Update (정렬)가장 큰 수.py

--- Lv2/(정렬)가장 큰 수.py
+++ Lv2/(정렬)가장 큰 수.py
@@ -1,4 +1,5 @@
 # 시간 초과
+# permultations으로 푸려고 했으나 시간초과로 실패
 
 from itertools import permutations
 def solution(numbers):
@@ -10,4 +11,12 @@ def solution(numbers):
     
     return max(answer)
 --------------------------------------------------------------------------
+
+def solution(numbers):
+    answer = []
+    
+    num = list(map(str,numbers))
+    num.sort(key = lambda x : x*3, reverse = True)
+    # x*3을 하는 이유 : num의 인수값이 1000이하이므로 3자리수로 맞춘 뒤, 비교하겠다는 의미
+    return str(int("".join(num)))
 
