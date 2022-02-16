2282186a2b1b251f1ff52e72ae34e48b47a2a1bc
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-02-16 16:38:56 +0900

    Create 더 맵게(Heap).py

---
+++ Lv2/더 맵게(Heap).py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(scoville, K):
+    scoville.sort()
+    heapq.heapify(scoville)
+    answer = 0
+
+    while scoville[0] <= K:
+        if len(scoville) == 1:
+            return -1
+        mix =heapq.heappop(scoville) + heapq.heappop(scoville)*2
+        heapq.heappush(scoville, mix)
+
+        answer += 1
+
+    return answer
