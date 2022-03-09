24702e6c79091d7ed3141fb8154b412d8511c5d8
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-03-09 14:53:40 +0900

    Update 소수 찾기.py

--- Lv1/소수 찾기.py
+++ Lv1/소수 찾기.py
@@ -22,19 +22,16 @@ def solution(n):
 
 import math
 
-n = 1000	# 2부터 1000까지 모든 수에 대하여 소수를 찾을 것이다.
-array = [True for i in range(n + 1) # 0,1을 제외한 모든 숫자가 소수(True)인 것으로 설정하고 시작한다.
+def Prime_num(num):
+    if num==1:
+        return False
+    else:
+        for n in range(2, int(num**0.5)+1):	# n의 제곱근까지만 
+            if num % n ==0:
+                return False
+        return True
+M, N = map(int, input().split())
 
-# 에라토스테네스의 체 알고리즘
-for i in range(2, int(math.sqrt(n)) + 1): # 2부터 n의 제곱근까지의 모든 수를 확인
-	if array[i] == True:	# i가 소수인 경우
-    	# i를 제외한 i의 모든 배수를 지우기
-        j = 2
-        while i * j <= n:
-        	array[i * j] = False
-            j += 1
-            
-#모든 소수 출력
-for i range(2, n + 1):
-	if array[i]:
-    	print(i, end = ' ')
+for i in range(M, N+1):
+    if Prime_num(i):
+        print(i)
