91df973f5258abdbb993423dff0afdac654d1c91
Jisoo <90206705+JisooRyu99@users.noreply.github.com>
2022-06-10 15:09:18 +0900

    Create (Silver4) 수열 정렬.py

--- 백준/(Silver4) 수열 정렬.py
+++ 백준/(Silver4) 수열 정렬.py
@@ -6,6 +6,7 @@ result = []
 sorted_A = sorted(A)
 
 for i in A:
-    result.append(sorted_A.index(i))
-    sorted_A[sorted_A.index(i)] = -1
+    result.append(sorted_A.index(i))    # index 탐색
+    sorted_A[sorted_A.index(i)] = -1    # 탐색한 index는 다시 찾지 못하게 만듦
+  
 print(*result)
