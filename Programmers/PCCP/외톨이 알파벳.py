2f89d7249248b7e109ee80326049f0e1e70f533a
JisooRyu99 <jisoo@Jisoo-MacBook-Pro.local>
2023-03-27 01:25:26 +0900

    외톨이 알파벳

---
+++ Programmers/PCCP/외톨이 알파벳.py
@@ -0,0 +1,19 @@
+from collections import Counter
+def solution(input_string):
+    answer = set()
+    count = Counter(input_string)
+    stack = []
+    
+    for x in input_string:
+        if not stack: stack.append(x)
+        else:
+            if stack[-1] == x:
+                stack.append(x)
+            else:
+                if count[stack[-1]]!=len(stack):
+                    answer.add(stack[-1])
+                stack = [x]
+                
+    return "".join(sorted(answer)) if answer else 'N'
+
+# 문제  : https://school.programmers.co.kr/learn/courses/15008/lessons/121683?language=python3
