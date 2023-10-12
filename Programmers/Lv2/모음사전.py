87aa359b0f2ac396fa5b80b927eba9f14b71b6d4
JisooRyu99 <cussion09@gmail.com>
2023-10-12 18:52:34 +0900

    2023.10.12

---
+++ Programmers/Lv2/모음사전.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+from itertools import product
+def solution(word):
+    answer = []
+    
+    for i in range(1,6):
+        for j in product(['A','E','I','O','U'], repeat=i):
+            answer.append("".join(j))
+    answer.sort()
+    
+    return answer.index(word)+1
