3e9284477b503a8f71a13e98df117bc187a662fd
JisooRyu99 <cussion09@gmail.com>
2023-11-04 21:17:46 +0900

    2023.11.04

---
+++ BOJ/1300. K번째 수.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/1300
+
+N = int(input())
+k = int(input())
+
+start, end = 1, k
+
+while start<=end:
+    mid = (start+end)//2
+    
+    tmp=0
+    for i in range(1,N+1):
+        tmp += min(mid//i, N)
+        
+    if tmp >= k:
+        answer = mid
+        end = mid-1
+    else:
+        start = mid+1
+print(answer)
